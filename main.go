0af7b634e418c7874cde2ad7411061f4161e1a7d
Jean de Klerk <deklerk@google.com>
2019-09-23 20:20:26 -0600

    better docs

--- main.go
+++ main.go
@@ -1,31 +1,86 @@
 /*
 nmod provides support for operations on submodules.
 
-Usage:
-        nmod <command> [arguments]
+Usage: nmod <command> [args...]
+
+NOTE: nmod is built to be run at the root of the repository it is operating on.
+It does NOT query for modules - it just scans directories in a straight line
+above, and recursively below, the working directory.
 
 The commands are:
+	modules			print the modules of the given dirs
+	rootdirs		print the root dirs of the given modules
+	dirs			print the dirs of the given modules
 
-modules         print the modules in this repository
-dir             print the root directory of a module in this repository
-dirs            print the directories of a module in this repository
+modules:
+	nmod modules [dirs...]
+
+modules prints the modules of the dirs if they're supplied. Dirs may be supplied
+as space separated arguments or as STDIN. If no dirs are supplied, modules
+prints the module of the current directory (if it exists) and all modules in
+directories recursively below the current directory.
+
+rootdirs:
+	mmod rootdirs [modules...]
+
+rootdirs prints the root directories of the given modules. Modules may be
+supplied as space separated arguments or as STDIN. If no modules are supplied,
+rootdirs prints the root directory of the module of the current directory (if
+it exists).
+
+dirs:
+	mmod dirs [modules...]
+
+dirs prints the directories belonging to the given modules. Modules may be
+supplied as space separated arguments or as STDIN. If no modules are supplied,
+dirs prints the root directory of the module of the current directory (if
+it exists).
 */
 
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, `Usage: nmod <command> [arguments]
+	fmt.Fprintf(os.Stderr, `Usage: nmod <command> [args...]
+
+NOTE: nmod is built to be run at the root of the repository it is operating on.
+It does NOT query for modules - it just scans directories in a straight line
+above, and recursively below, the working directory.
 
 The commands are:
-	modules         print the modules in this repository
-	dir             print the root directory of a module in this repository
-	dirs            print the directories of a module in this repository
+	modules			print the modules of the given dirs
+	rootdirs		print the root dirs of the given modules
+	dirs			print the dirs of the given modules
+
+modules:
+	nmod modules [dirs...]
+
+modules prints the modules of the dirs if they're supplied. Dirs may be supplied
+as space separated arguments or as STDIN. If no dirs are supplied, modules
+prints the module of the current directory (if it exists) and all modules in
+directories recursively below the current directory.
+
+rootdirs:
+	mmod rootdirs [modules...]
+
+rootdirs prints the root directories of the given modules. Modules may be
+supplied as space separated arguments or as STDIN. If no modules are supplied,
+rootdirs prints the root directory of the module of the current directory (if
+it exists).
+
+dirs:
+	mmod dirs [modules...]
+
+dirs prints the directories belonging to the given modules. Modules may be
+supplied as space separated arguments or as STDIN. If no modules are supplied,
+dirs prints the root directory of the module of the current directory (if
+it exists).
 `)
 	os.Exit(2)
 }
@@ -46,5 +101,26 @@ func main() {
 }
 
 func nmod(cmd string, args []string) error {
+	switch cmd {
+	case "modules":
+		return errors.New("modules unimplemented")
+	case "dir":
+		return errors.New("dir unimplemented")
+	case "dirs":
+		return errors.New("dirs unimplemented")
+	case "help":
+		usage()
+	default:
+		usage()
+	}
+
 	return nil
+}
+
+func modules() {
+
+}
+
+func findProjectRoot() {
+
 }
