411f154267b6c9d12e69c1a313f2b2487bf45ac1
Jean de Klerk <deklerk@google.com>
2019-09-23 21:11:22 -0600

    flesh out modules: add directory listing

--- main.go
+++ main.go
@@ -101,6 +101,9 @@ func main() {
 func nmod(cmd string, args []string) error {
 	switch cmd {
 	case "modules":
+		if len(args) == 0 {
+			return allModules()
+		}
 		return modules(args)
 	case "rootdirs":
 		return rootdirs(args)
@@ -115,28 +118,55 @@ func nmod(cmd string, args []string) error {
 	return nil
 }
 
-var moduleRegexp = regexp.MustCompile("module (.+)")
+func modules(dirs []string) error {
+	modFiles := map[string]struct{}{}
 
-func modules(args []string) error {
-	var modFiles []string
+	for _, d := range dirs {
+		// Pessimistically assume user didn't provide an absolute path - convert
+		// every path into an absolute path.
+		absD, err := filepath.Abs(d)
+		if err != nil {
+			return err
+		}
+		d = absD
+
+		// Go up from specified directory until we see a go.mod.
+		m, err := searchUpwardsForModule(d)
+		if err != nil {
+			return err
+		}
+		if m == "" {
+			return fmt.Errorf("%s doesn't have a go.mod, nor do any of the directories above it", d)
+		}
+		modFiles[m] = struct{}{}
+	}
+
+	modules := map[string]struct{}{}
+	for f := range modFiles {
+		m, err := readModule(f)
+		if err != nil {
+			return err
+		}
+		modules[m] = struct{}{}
+	}
+
+	for m := range modules {
+		fmt.Println(m)
+	}
+
+	return nil
+}
+
+func allModules() error {
+	modFiles := map[string]struct{}{}
 
 	// Go up until we see a go.mod.
-	var d string
-	for cur := "."; d != "/"; cur += "/.." {
-		var err error
-		d, err = filepath.Abs(cur)
-		if err != nil {
-			return err
-		}
-		modFile := d + "/go.mod"
-		_, err = os.Stat(modFile)
-		if err != nil {
-			if os.IsNotExist(err) {
-				continue
-			}
-			return err
-		}
-		modFiles = append(modFiles, modFile)
+	m, err := searchUpwardsForModule(".")
+	if err != nil {
+		return err
+	}
+	if m != "" {
+		modFiles[m] = struct{}{}
 	}
 
 	// Now go recursively down collecting go.mod files.
@@ -145,31 +175,65 @@ func modules(args []string) error {
 			return err
 		}
 		if info.Name() == "go.mod" {
-			modFiles = append(modFiles, path)
+			modFiles[path] = struct{}{}
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
 
-	dedupedMatches := map[string]struct{}{}
-	for _, f := range modFiles {
-		outbytes, err := ioutil.ReadFile(f)
+	modules := map[string]struct{}{}
+	for f := range modFiles {
+		m, err := readModule(f)
 		if err != nil {
 			return err
 		}
-		matches := moduleRegexp.FindAllStringSubmatch(string(outbytes), -1)
-		if len(matches) == 0 {
-			return fmt.Errorf("%s doesn't seem to have a module declaration", f)
-		}
-		dedupedMatches[matches[0][1]] = struct{}{}
+		modules[m] = struct{}{}
 	}
 
-	for m := range dedupedMatches {
+	for m := range modules {
 		fmt.Println(m)
 	}
 
 	return nil
+}
+
+// searchUpwardsForModule searches each directory above the given startDir for
+// a go.mod file. It returns the file location of the go.mod.
+func searchUpwardsForModule(startDir string) (string, error) {
+	var absCurDir string
+	for curDir := startDir; absCurDir != "/"; curDir += "/.." {
+		var err error
+		absCurDir, err = filepath.Abs(curDir)
+		if err != nil {
+			return "", err
+		}
+		modFile := absCurDir + "/go.mod"
+		_, err = os.Stat(modFile)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+			return "", err
+		}
+		return modFile, nil
+	}
+	return "", nil
+}
+
+var moduleRegexp = regexp.MustCompile("module (.+)")
+
+// readModule reads a given go.mod file and returns its module name.
+func readModule(f string) (string, error) {
+	outbytes, err := ioutil.ReadFile(f)
+	if err != nil {
+		return "", err
+	}
+	matches := moduleRegexp.FindAllStringSubmatch(string(outbytes), -1)
+	if len(matches) == 0 {
+		return "", fmt.Errorf("%s doesn't seem to have a module declaration", f)
+	}
+	return matches[0][1], nil
 }
 
 func rootdirs(args []string) error {
